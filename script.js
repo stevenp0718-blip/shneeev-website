9ecf09c79aef2c12cd9775d5c60a846bde5a53a9
stevenp0718-blip <stevenp0718@gmail.com>
2026-08-01 04:53:04 -0700

    Set up protected Resend newsletter automation

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
     scheduleIdleWork();
     setupEmailGlow();
     setupShoppingLinks();
-    setupNewsletterForm();
     setupNewsletterTypingPrompt();
     setupReviewSearch();
     scheduleVideoLoad();
